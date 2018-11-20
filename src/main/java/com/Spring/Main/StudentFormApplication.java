9ff96bbc0769a66d814141f790f2422f8d481bc4
Rohit Agarwal <Rohit.Agarwal@igglobal.com>
2018-11-20 14:04:15 +0530

    hello

---
+++ src/main/java/com/Spring/Main/StudentFormApplication.java
@@ -0,0 +1,12 @@
+package com.Spring.Main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentFormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentFormApplication.class, args);
+	}
+}
---
+++ src/test/java/com/Spring/Main/StudentFormApplicationTests.java
@@ -0,0 +1,16 @@
+package com.Spring.Main;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StudentFormApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
